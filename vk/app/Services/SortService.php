11da45367290bfe5b628d66c8f57324e0f8ed52d
Budalov-Dmitry <northlands91@gmail.com>
2023-04-09 21:45:29 +0300

    Highload

--- vk/app/Services/SortService.php
+++ vk/app/Services/SortService.php
@@ -10,13 +10,29 @@ class SortService implements Sort
     public function  sort(array $array)
     {
 
+//        $value = 'value1';
+//        if((int)$value == 0)
+//        {
+//            echo 1;
+//        }
+
+//        $shift = 111;
+//        $count = count($array);
+//
+//        $shift = $shift % $count;
+//
+//        $item = array_splice($array,-$shift);
+//
+//        $result = array_merge($item,$array);
+//        var_dump($result);
+
+
         $start = memory_get_usage();
         Log::info('Начало сортировки '.$start);
         for($i=0; $i<count($array); $i++){
             $count = count($array);
             for($j=$i+1; $j<$count; $j++){
                 if($array[$i]>$array[$j]){
-
                     $temp = $array[$j];
                     $array[$j] = $array[$i];
                     $array[$i] = $temp;
@@ -30,5 +46,12 @@ class SortService implements Sort
         Log::info('Конец сортировки '. $end);
 
         return $result;
+    }
+
+    public function myMagicSummator()
+    {
+        static $id = 0;
+        $id++;
+        echo $id;
     }
 }
